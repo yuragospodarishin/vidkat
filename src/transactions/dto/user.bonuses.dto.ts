8507486d26afdc91e648bb5433bc66a446e34347
yuragospodarishin <yurkagospodarishinwork@ukr.ent>
2022-05-28 13:09:28 -0700

    asdasdsadsad

--- src/transactions/dto/user.bonuses.dto.ts
+++ src/transactions/dto/user.bonuses.dto.ts
@@ -2,25 +2,25 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, Min } from 'class-validator';
 
 export class UserBonusesDto {
-  @ApiProperty({ example: 13, description: 'transaction total' })
+  @ApiProperty({ example: 40, description: 'transaction total' })
   @IsNotEmpty({ message: 'total must be not empty' })
   @IsNumber()
   @Min(1)
   total: number;
 
-  @ApiProperty({ example: 13, description: 'transaction personal' })
+  @ApiProperty({ example: 10, description: 'transaction personal' })
   @IsNotEmpty({ message: 'personal must be not empty' })
   @IsNumber()
   @Min(1)
   personal: number;
 
-  @ApiProperty({ example: 13, description: 'transaction referral' })
+  @ApiProperty({ example: 10, description: 'transaction referral' })
   @IsNotEmpty({ message: 'referral must be not empty' })
   @IsNumber()
   @Min(1)
   referral: number;
 
-  @ApiProperty({ example: 13, description: 'transaction extra' })
+  @ApiProperty({ example: 12, description: 'transaction extra' })
   @IsNotEmpty({ message: 'extra must be not empty' })
   @IsNumber()
   @Min(1)
