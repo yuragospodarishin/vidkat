e38dccfc436212a111507316ab1b954dc93e7baa
yuragospodarishin <yurkagospodarishinwork@ukr.ent>
2022-05-28 08:15:40 -0700

    test123

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,10 @@ export class PostgresDBConfigService implements TypeOrmOptionsFactory {
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
     return {
-      url: this.configService.get<string>('URL_DB_STAGE'),
+      type: 'postgres',
+      synchronize: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      url: 'postgres://bncuyzyvhgornd:8267365c8e8d32fd983917069ae04ef33df1ab721e5b80993d6e19002eb4278d@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/daoqc8oal2rmsh',
       ssl: {
         rejectUnauthorized: false,
       },
@@ -17,9 +20,6 @@ export class PostgresDBConfigService implements TypeOrmOptionsFactory {
       // username: this.configService.get<string>('POSTGRES_USERNAME'),
       // password: this.configService.get<string>('POSTGRES_PASSWORD'),
       // database: this.configService.get<string>('POSTGRES_DB'),
-      type: 'postgres',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
     };
   }
 }
