f896df1dc0c4c476e2c0564e8f33407f06cfd157
yuragospodarishin <yurkagospodarishinwork@ukr.ent>
2022-05-28 09:04:31 -0700

    asdasm,lme33

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,55 +1,95 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule } from '@nestjs/config';
+// import { UserModule } from './user/user.module';
+// import { AuthModule } from './auth/auth.module';
+// import { TransactionsModule } from './transactions/transactions.module';
+// import { UserEntity } from './user/user.entity';
+// import { BaseEntity } from './entitys/base.entity';
+// import { TokensEntity } from './auth/tokens.entity';
+// import { TransactionsEntity } from './transactions/transactions.entity';
+// import { FeedbackModule } from './feedback/feedback.module';
+//
+// @Module({
+//   imports: [
+//     UserModule,
+//     AuthModule,
+//     TransactionsModule,
+//     FeedbackModule,
+//     TypeOrmModule.forFeature([BaseEntity, TokensEntity, UserEntity, TransactionsEntity]),
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       synchronize: true,
+//       entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//       url: 'postgres://bncuyzyvhgornd:8267365c8e8d32fd983917069ae04ef33df1ab721e5b80993d6e19002eb4278d@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/daoqc8oal2rmsh',
+//       ssl: {
+//         rejectUnauthorized: false,
+//       },
+//       // host: 'localhost',
+//       // port: 5433,
+//       // username: 'yura',
+//       // password: '0000',
+//       // database: 'vidkatapi',
+//     }),
+//     // TypeOrmModule.forRootAsync({
+//     //   useFactory: () => ({
+//     //     type: 'postgres',
+//     //     host: 'localhost',
+//     //     port: 5433,
+//     //     username: 'yura',
+//     //     password: '0000',
+//     //     database: 'vidkatapi',
+//     //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+//     //     synchronize: true,
+//     //   }),
+//     // }),
+//     ConfigModule.forRoot({
+//       envFilePath: ['./.env'],
+//     }),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService, ConfigModule],
+// })
+// export class AppModule {}
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { TransactionsModule } from './transactions/transactions.module';
-import { UserEntity } from './user/user.entity';
-import { BaseEntity } from './entitys/base.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { PostgresDBConfigService } from './ormconfig';
 import { TokensEntity } from './auth/tokens.entity';
+import { UserEntity } from './user/user.entity';
 import { TransactionsEntity } from './transactions/transactions.entity';
+import { FeedbackEntity } from './feedback/feedback.entity';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 import { FeedbackModule } from './feedback/feedback.module';
+import { TransactionsModule } from './transactions/transactions.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { createConnection } from 'typeorm';
 
 @Module({
   imports: [
-    UserModule,
     AuthModule,
-    TransactionsModule,
+    UserModule,
     FeedbackModule,
-    TypeOrmModule.forFeature([BaseEntity, TokensEntity, UserEntity, TransactionsEntity]),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      url: 'postgres://bncuyzyvhgornd:8267365c8e8d32fd983917069ae04ef33df1ab721e5b80993d6e19002eb4278d@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/daoqc8oal2rmsh',
-      ssl: {
-        rejectUnauthorized: false,
-      },
-      // host: 'localhost',
-      // port: 5433,
-      // username: 'yura',
-      // password: '0000',
-      // database: 'vidkatapi',
-    }),
-    // TypeOrmModule.forRootAsync({
-    //   useFactory: () => ({
-    //     type: 'postgres',
-    //     host: 'localhost',
-    //     port: 5433,
-    //     username: 'yura',
-    //     password: '0000',
-    //     database: 'vidkatapi',
-    //     entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    //     synchronize: true,
-    //   }),
-    // }),
+    TransactionsModule,
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forFeature([TokensEntity, UserEntity, TransactionsEntity, FeedbackEntity]),
     ConfigModule.forRoot({
-      envFilePath: ['./.env'],
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresDBConfigService,
+      inject: [PostgresDBConfigService, ConfigService],
+      imports: [ConfigModule],
+      connectionFactory: async (options) => {
+        const connection = await createConnection(options);
+        return connection;
+      },
     }),
   ],
-  controllers: [AppController],
-  providers: [AppService, ConfigModule],
+  providers: [ConfigService],
 })
 export class AppModule {}
