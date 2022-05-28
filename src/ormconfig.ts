fa9a084024391b34d8898fdbf4bfadb83ed03f49
yuragospodarishin <yurkagospodarishinwork@ukr.ent>
2022-05-28 08:44:16 -0700

    adssavsdffdfsf

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -12,9 +12,11 @@ export class PostgresDBConfigService implements TypeOrmOptionsFactory {
       synchronize: true,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       url: 'postgres://bncuyzyvhgornd:8267365c8e8d32fd983917069ae04ef33df1ab721e5b80993d6e19002eb4278d@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/daoqc8oal2rmsh',
-      extra: { ssl: true },
-      ssl: {
-        rejectUnauthorized: false,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       // host: this.configService.get<string>('POSTGRES_HOST'),
       // port: this.configService.get<number>('POSTGRES_PORT'),
