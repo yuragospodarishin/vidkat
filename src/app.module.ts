342755eab9c1958a380fffdaeb370cd35d657a54
yuragospodarishin <yurkagospodarishinwork@ukr.ent>
2022-05-28 09:45:55 -0700

    sadlmdsma2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TransactionsModule } from './transactions/transactions.module';
@@ -11,9 +11,13 @@ import { BaseEntity } from './entitys/base.entity';
 import { TokensEntity } from './auth/tokens.entity';
 import { TransactionsEntity } from './transactions/transactions.entity';
 import { FeedbackModule } from './feedback/feedback.module';
+import { ScheduleModule } from '@nestjs/schedule';
+import { PostgresDBConfigService } from './ormconfig';
+import { createConnection } from 'typeorm';
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     UserModule,
     AuthModule,
     TransactionsModule,
@@ -34,15 +38,27 @@ import { FeedbackModule } from './feedback/feedback.module';
     //   // database: 'vidkatapi',
     // }),
     TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'postgres',
-        synchronize: true,
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        url: 'postgres://bncuyzyvhgornd:8267365c8e8d32fd983917069ae04ef33df1ab721e5b80993d6e19002eb4278d@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/daoqc8oal2rmsh',
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }),
+      useClass: PostgresDBConfigService,
+      inject: [PostgresDBConfigService, ConfigService],
+      imports: [ConfigModule],
+      connectionFactory: async (options) => {
+        const connection = await createConnection(options);
+        return connection;
+      },
+      // useFactory: () => ({
+      //   // type: 'postgres',
+      //   // synchronize: true,
+      //   // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      //   // url: 'postgres://bncuyzyvhgornd:8267365c8e8d32fd983917069ae04ef33df1ab721e5b80993d6e19002eb4278d@ec2-54-228-218-84.eu-west-1.compute.amazonaws.com:5432/daoqc8oal2rmsh',
+      //   // ssl: {
+      //   //   rejectUnauthorized: false,
+      //   // },
+      //   // host: 'localhost',
+      //   // port: 5433,
+      //   // username: 'yura',
+      //   // password: '0000',
+      //   // database: 'vidkatapi',
+      // }),
     }),
     ConfigModule.forRoot({
       envFilePath: ['./.env'],
